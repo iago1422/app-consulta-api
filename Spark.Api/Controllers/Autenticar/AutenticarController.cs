93ca4760406d0d86548dd8cb55e3a826983ed46e
iago1422 <iagosoares517@gmail.com>
2026-01-08 17:45:26 -0300

    getAll ficha

--- Spark.Api/Controllers/Autenticar/AutenticarController.cs
+++ Spark.Api/Controllers/Autenticar/AutenticarController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Spark.Domain.Commands;
 using Spark.Domain.Commands.Autenticar;
